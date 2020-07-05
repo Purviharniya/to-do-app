dc15ce05fe6a3faf4937d693cb89c141bc783605
Purviharniya <purvi.harniya@gmail.com>
2020-07-05 12:28:34 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './App.css';
+import ListItems from './ListItem'
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faTrash);
+
+class App extends React.Component {
+
+  constructor(props){
+    super();
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:'',
+      }
+    }
+    this.handleInput=this.handleInput.bind(this);
+    this.addItem=this.addItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now(),
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem= this.state.currentItem;
+    // console.log(newItem);
+    if(newItem.text!==''){
+      const newItems=[...this.state.items,newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:'',
+        }
+      })
+    }
+  }
+
+ deleteItem= (key)=>{
+    const filterItems=this.state.items.filter(item=> item.key!==key);
+    this.setState({items:filterItems})
+  }
+
+  setUpdate=(text,key) =>{
+    const items=this.state.items;
+    items.map(item =>{
+      if(item.key===key){
+        item.text=text;
+      }
+    })
+
+    this.setState({items: items})
+  }
+
+  render(){
+  
+    return (
+     <div className="App">
+       <header>
+       <form id="to-do-form" onSubmit={this.addItem}>
+         <input type="text" placeholder="Enter task...." value={this.state.currentItem.text} onChange={this.handleInput}/>
+         <button type="submit" >ADD</button>
+       </form>
+     </header>
+     <ListItems items={this.state.items}  deleteItem={this.deleteItem} setUpdate={this.setUpdate}></ListItems>
+     </div>
+    );
+  }
+}
+
+export default App;
